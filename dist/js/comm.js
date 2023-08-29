0044ffec5c88aff088e1e9f212fbdd771e7b3af8
zzining <iammes98@gmail.com>
2023-08-29 18:05:12 +0900

    slide_fin

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -1,33 +1,52 @@
-let photoSwiper = new Swiper(".swiper.is-photo", {
-  effect: "flip",
-  loop: true,
-  grabCursor: true,
-  speed: 1000,
-  slidesPerView: 1, // 콘텐츠 슬라이더도 1개씩 보이도록 설정
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+$(function () {
+    let photoSwiper = new Swiper('.swiper.is-photo', {
+        effect: 'flip',
+        loop: true,
+        grabCursor: true,
+        speed: 1000,
+        slidesPerView: 1, // 콘텐츠 슬라이더도 1개씩 보이도록 설정
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
 
-let contentSwiper = new Swiper(".swiper.is-content", {
-  effect: "fade",
-  fadeEffect: {
-    crossFade: true,
-  },
-  loop: true,
-  grabCursor: true,
-  speed: 1000,
-  slidesPerView: 1, // 콘텐츠 슬라이더도 1개씩 보이도록 설정
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+    let contentSwiper = new Swiper('.swiper.is-content', {
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true,
+        },
+        loop: true,
+        grabCursor: true,
+        speed: 1000,
+        slidesPerView: 1, // 콘텐츠 슬라이더도 1개씩 보이도록 설정
+        navigation: {
+            nextEl: '.swiper-main-next',
+            prevEl: '.swiper-main-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
 
-photoSwiper.controller.control = contentSwiper;
-contentSwiper.controller.control = photoSwiper;
+    photoSwiper.controller.control = contentSwiper;
+    contentSwiper.controller.control = photoSwiper;
+
+    $('.slider_pre-tag a').on('click', function () {
+        $('.slider_pre_tagline').removeClass('on');
+    });
+    $('.slider_pre-tag a').on('click', function () {
+        $(this).addClass('on');
+    });
+
+    var preSwiper = new Swiper('.swiper.is-preimg', {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        grabCursor: true,
+        navigation: {
+            nextEl: '.swiper-arrow-next',
+            prevEl: '.swiper-arrow-prev',
+        },
+    });
+});
